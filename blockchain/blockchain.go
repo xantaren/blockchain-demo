f4bf63d0c5d999764d4e280cc21b06d4a4245db8
Jayson <xantaren@gmail.com>
2022-10-25 22:46:47 +0800

    Import BadgerDB

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,15 +1,18 @@
 package blockchain
 
+import "github.com/dgraph-io/badger"
+
 type BlockChain struct {
-	Blocks []*Block
+	Blocks   []*Block
+	Database *badger.DB
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
 }
 
 func (chain *BlockChain) AddBlock(data string) {
 	prevBlock := chain.Blocks[len(chain.Blocks)-1]
 	newBlock := CreateBlock(data, prevBlock.Hash)
 	chain.Blocks = append(chain.Blocks, newBlock)
-}
-
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
 }
