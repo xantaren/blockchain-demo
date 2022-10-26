19f1320904d9a6853b19c847a5f06d78dfecc5c0
Jayson <xantaren@gmail.com>
2022-10-26 21:28:27 +0800

    Refactor for persistent storage

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,18 +1,101 @@
 package blockchain
 
-import "github.com/dgraph-io/badger"
+import (
+	"github.com/dgraph-io/badger"
+	"log"
+)
+
+const (
+	DbPath      = "./tmp/blocks"
+	LastHashKey = "lastHash"
+)
 
 type BlockChain struct {
-	Blocks   []*Block
+	LastHash []byte
 	Database *badger.DB
 }
 
 func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
+	// Necessary for deriving a new block in the blockchain
+	var lastHash []byte
+
+	options := badger.DefaultOptions(DbPath)
+
+	db, err := badger.Open(options)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	err = db.Update(func(txn *badger.Txn) error {
+		if _, err = txn.Get([]byte(LastHashKey)); err == badger.ErrKeyNotFound {
+			genesis := Genesis()
+			err = txn.Set(genesis.Hash, genesis.Serialize())
+			if err != nil {
+				log.Panicln(err)
+			}
+
+			err = txn.Set([]byte(LastHashKey), genesis.Hash)
+			if err != nil {
+				log.Panicln(err)
+			}
+
+			lastHash = genesis.Hash
+
+			return err
+		} else {
+			item, err := txn.Get([]byte(LastHashKey))
+			if err != nil {
+				log.Panicln(err)
+			}
+
+			err = item.Value(func(val []byte) error {
+				lastHash = val
+				return nil
+			})
+			return err
+		}
+	})
+
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	blockchain := BlockChain{lastHash, db}
+	return &blockchain
 }
 
 func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	newBlock := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, newBlock)
+	var lastHash []byte
+
+	err := chain.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte(LastHashKey))
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		err = item.Value(func(val []byte) error {
+			lastHash = val
+			return nil
+		})
+		return err
+	})
+
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	newBlock := CreateBlock(data, lastHash)
+
+	err = chain.Database.Update(func(txn *badger.Txn) error {
+		err := txn.Set(newBlock.Hash, newBlock.Serialize())
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		err = txn.Set([]byte(LastHashKey), newBlock.Hash)
+
+		chain.LastHash = newBlock.Hash
+
+		return err
+	})
 }
